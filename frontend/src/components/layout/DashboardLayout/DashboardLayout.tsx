9428b6ba5734eb03a0088d6431107f97cbabf044
Calebe Felipe <fcalebe7@gmail.com>
2025-06-05 06:51:26 -0300

    feat: Implementando a estrutura básica do layout da página de Dashboard

---
+++ frontend/src/components/layout/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,23 @@
+import { Sidebar } from '../Sidebar'
+import type { DashboardLayoutProps } from "../../../types/dashboard";
+import './Dashboard.css'
+
+export const DashboardLayout = ({ children, title }: DashboardLayoutProps) => {
+
+    return (
+
+        <div className="dashboard-layout">
+            <Sidebar />
+
+            <main>
+                {title && <h1>{title}</h1>}
+
+                <div className="content">
+                    {children}
+                </div>
+            </main>
+        </div>
+
+    );
+
+}
